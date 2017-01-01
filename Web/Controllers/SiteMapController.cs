d2ad84f1b9fce9d1963b666ba620e7aa50424f47
The Rationalizer <TheRationaliser@gmail.com>
2017-01-01 18:05:59 +0000

    Update sitemap date

--- Web/Controllers/SiteMapController.cs
+++ Web/Controllers/SiteMapController.cs
@@ -11,7 +11,7 @@ namespace QuranX.Controllers
     {
 		const string Domain = "http://QuranX.com";
 		const int PageSize = 10 * 1000;
-		readonly DateTime LastMod = new DateTime(2014, 12, 07);
+		readonly DateTime LastMod = new DateTime(2017, 01, 01);
 
 		public SiteMapResult Quran()
 		{
