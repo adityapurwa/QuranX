ab57dc76aea895f0204ffb9c3e2d92d7928f4004
The Rationalizer <TheRationaliser@gmail.com>
2017-03-25 20:41:50 +0000

    Add chapters

--- QuranX.DomainLayer/ServicesImpl/ObjectSpace.cs
+++ QuranX.DomainLayer/ServicesImpl/ObjectSpace.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using QuranX.DomainLayer.DataModel;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QuranX.DomainLayer.ServicesImpl
 {
 	public class ObjectSpace : DbContext
 	{
 		public ObjectSpace() : base("DefaultConnection") { }
+
+		public DbSet<Chapter> Chapters { get; set; }
 	}
 }
